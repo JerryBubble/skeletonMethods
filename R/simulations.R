88fcfdaf48fa94c8fe815bb324df7938928932f5
Zeyu Wei <zwei5@uw.edu>
2022-07-27 17:41:58 -0700

    initial

--- R/simulations.R
+++ R/simulations.R
@@ -256,3 +256,141 @@ MM_data = function(n1=800,n2=400,n3=2000,sd1=0.04,sd2=0.04,sd3=0.04, d=3, sd_hig
   Y0 = c(rep(1,n1), rep(2,n2), rep(3,n3))
   return(list(data = X0, clus = Y0))
 }
+
+
+#' Generating SwissRoll Regression data
+#'
+#' @param N the sample size
+#' @param Height the maximum radius
+#' @param start the starting angle for the Swiss Roll
+#' @param roll the number of rolls
+#' @param d a number indicating the dimension of the generated data. d>3 dimensions are filled with independent Gaussian noises
+#' @param sd_high a number indicating the standard deviation for Gaussian noises in the higher dimensions
+#' @param sd_e a number indicating the standard deviation for the additive Gaussian noises in the response
+#' @return A list of generated data points and the true labels
+#' \itemize{
+#'   \item data - The generated data matrix
+#'   \item clus - The vector of true cluster labels for each data point
+#' }
+#' @export
+#' @examples
+#' X = MM_data()$data
+SwissRollReg <- function(N=2000, Height=4*pi,start = pi, roll=1, d = 3, sd_high=0.1, sd_e = 0.3){
+
+  ## build manifold
+  c = log((start + 2*pi*roll)/start) #need start > 0
+  p = start * exp(c *runif(N, 0, 1))
+  y = Height * runif(N, 0 , 1)
+  X = cbind(p * cos(p), y, p * sin(p))
+  X = scale(X)
+
+  if(d==3){
+    X0 = X
+  }
+  if(d>3){
+    n = nrow(X)
+    d_add = d-3
+    X0 = cbind(X, matrix(stats::rnorm(d_add*(n),  sd = sd_high), nrow = n))
+  }
+  p0 = p - start -  pi * roll
+  Y0 = 0.1*p0^3*( (y<pi) + (y > 2*pi & y<3*pi) ) + rnorm(N, mean = 0, sd = sd_e)
+
+  return(list(data = X0, Y = Y0, angles = p, heights = y))
+
+}
+
+
+
+#' Generating Yinyang regression data
+#'
+#' @param n_m number of points in each of the two moon shapes. Positive integer value or vector with length=2
+#' @param n_c number of points in each of the two small circles
+#' @param n_r number of points in the outer ring
+#' @param sd_c the standard deviation of shifts for points in the two small circles
+#' @param sd_r the standard deviation of shifts for points in the outer ring
+#' @param d a number indicating the dimension of the generated data. d>2 dimensions are filled with independent Gaussian noises
+#' @param sd_high a number indicating the standard deviation for Gaussian noises in the higher dimensions
+#' @return A list of generated data points and the true labels
+#' \itemize{
+#'   \item data - The generated data matrix
+#'   \item clus - The vector of true cluster labels for each data point
+#' }
+#' @export
+#' @examples
+#' X = Yinyang_data()$data
+#' plot(X[,1], X[,2])
+Yinyang_reg_data = function(n_m=400,n_c=200,n_r=2000,sd_c=0.1,sd_r=0.1, d=2, sd_high=0.1, sd_e = 0.1){
+
+  X_m = clusterSim::shapes.two.moon(n_m)
+
+  x1 = cbind(stats::rnorm(n_c, mean= 0.5, sd=sd_c), stats::rnorm(n_c, mean= -1.5, sd=sd_c))
+  x2 = cbind(stats::rnorm(n_c, mean= -1, sd=sd_c), stats::rnorm(n_c, mean= 0.5, sd=sd_c))
+
+  th = stats::runif(n_r,0,2*pi)
+  x  = 2.5*cos(th) - 0.25
+  y  = 2.5*sin(th) -0.5
+  x3  = cbind(x,y) + matrix(stats::rnorm(2*length(x), sd=sd_r), ncol=2)
+  Y_r = sin(th*4) + 1.5
+
+  X = rbind(X_m$data, x1, x2, x3)
+
+  if(d==2){
+    X0 = X
+  }
+  if(d>2){
+    n = nrow(X)
+    d_add = d-2
+    X0 = cbind(X, matrix(stats::rnorm(d_add*(n),  sd = sd_high), nrow = n))
+  }
+  Y0 = c(X_m$clusters, rep(0,n_c), rep(3,n_c), Y_r) + rnorm(nrow(X_m$data)+2*n_c+n_r, mean = 0, sd = sd_e)
+  return(list(data = X0, Y = Y0))
+
+}
+
+#' Generating Yinyang regression data
+#'
+#' @param n_m number of points in each of the two moon shapes. Positive integer value or vector with length=2
+#' @param n_c number of points in each of the two small circles
+#' @param n_r number of points in the outer ring
+#' @param sd_c the standard deviation of shifts for points in the two small circles
+#' @param sd_r the standard deviation of shifts for points in the outer ring
+#' @param d a number indicating the dimension of the generated data. d>2 dimensions are filled with independent Gaussian noises
+#' @param sd_high a number indicating the standard deviation for Gaussian noises in the higher dimensions
+#' @return A list of generated data points and the true labels
+#' \itemize{
+#'   \item data - The generated data matrix
+#'   \item clus - The vector of true cluster labels for each data point
+#' }
+#' @export
+#' @examples
+#' X = Yinyang_data()$data
+#' plot(X[,1], X[,2])
+NoiseYinyang_reg_data = function(nnoise = 800, n_m=400,n_c=200,n_r=2000,sd_c=0.1,sd_r=0.1, d=2, sd_high=0.1, sd_e = 0.1){
+
+  X_m = clusterSim::shapes.two.moon(n_m)
+
+  x1 = cbind(stats::rnorm(n_c, mean= 0.5, sd=sd_c), stats::rnorm(n_c, mean= -1.5, sd=sd_c))
+  x2 = cbind(stats::rnorm(n_c, mean= -1, sd=sd_c), stats::rnorm(n_c, mean= 0.5, sd=sd_c))
+
+  th = stats::runif(n_r,0,2*pi)
+  x  = 2.5*cos(th) - 0.25
+  y  = 2.5*sin(th) -0.5
+  x3  = cbind(x,y) + matrix(stats::rnorm(2*length(x), sd=sd_r), ncol=2)
+  Y_r = sin(th*4) + 1.5
+
+  x4 = cbind( stats::runif(nnoise, -3.5,3.5), stats::runif(nnoise, -3.5,3.5))
+
+  X = rbind(X_m$data, x1, x2, x3, x4)
+
+  if(d==2){
+    X0 = X
+  }
+  if(d>2){
+    n = nrow(X)
+    d_add = d-2
+    X0 = cbind(X, matrix(stats::rnorm(d_add*(n),  sd = sd_high), nrow = n))
+  }
+  Y0 = c(X_m$clusters, rep(0,n_c), rep(3,n_c), Y_r, rep(1.5,nnoise)) + rnorm(nrow(X_m$data)+2*n_c+n_r+nnoise, mean = 0, sd = sd_e)
+  return(list(data = X0, Y = Y0))
+
+}
